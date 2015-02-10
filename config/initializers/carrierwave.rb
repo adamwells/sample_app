2ea6cb7a1b6a26d1e6000f666b4db3d2628c3d20
adamwellsdev <adamwells.dev@gmail.com>
2015-02-10 21:07:00 +0000

    Fixing carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIUNH3ZS4T5YTVVJA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['adamwellsdevrailstutorial']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
